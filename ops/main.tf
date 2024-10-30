8edceb02dcb4119d1e393d2e387b9ef003cc17ff
DariuszMech <dariuszmech@10g.pl>
2024-10-30 16:29:58 +0100

    created terraform script for bot service resource creation

--- ops/main.tf
+++ ops/main.tf
@@ -1,9 +1,12 @@
-# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "3.0.2"
     }
   }
 
@@ -11,11 +14,61 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azuread" {
+  tenant_id = data.azurerm_client_config.current.tenant_id
+}
+
+# Retrieve current Azure client configuration
+data "azurerm_client_config" "current" {}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = "TerraformOCB"
   location = "westus2"
 }
 
+
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "${azurerm_resource_group.rg.name}-WebApp-ASP-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"
+  sku_name            = "F1"
+
+}
+
+resource "azurerm_windows_web_app" "app_service" {
+  name                = "${azurerm_resource_group.rg.name}-WebApp-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
+
+  site_config {
+    minimum_tls_version = "1.2"
+    always_on           = false
+  }
+}
+
+resource "azuread_application_registration" "example" {
+  display_name     = "${azurerm_resource_group.rg.name}"
+}
+
+# Configure the Azure Bot Service using the new App Registration details
+resource "azurerm_bot_service_azure_bot" "example" {
+  name                    = "${azurerm_resource_group.rg.name}-Bot-${random_integer.ri.result}"
+  resource_group_name     = azurerm_resource_group.rg.name
+  location                = "global"
+  microsoft_app_id        = azuread_application_registration.example.client_id
+  sku                     = "F0"
+}
